2ab6ddb9ed7dcba544e37fe7fa6c0cf83e7d3052
Daniyal45 <daniyal_09.2005@hotmail.com>
2021-09-16 18:55:16 +0500

    view timesheet done

--- client/src/CreateSheet/CreateSheet.js
+++ client/src/CreateSheet/CreateSheet.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {HashRouter as Router, Route, Link} from 'react-router-dom';
-import {AnalogTime} from 'react-clock-select';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { AnalogTime } from 'react-clock-select';
 import toast, { Toaster } from 'react-hot-toast';
 import Table from 'react-table-lite';
 import { SERVER } from './../assets/scripts/constants';
@@ -11,25 +11,25 @@ export default class CreateSheet extends Component {
 
     constructor(props) {
         super(props)
-    
-        this.state = {             
-             projects:[],
-             tasks:[],
-             date: new Date().toISOString().slice(0, 10)
+
+        this.state = {
+            projects: [],
+            tasks: [],
+            date: new Date().toISOString().slice(0, 10)
         }
     }
-    
-    componentDidMount(){
-        this.initSheet();        
+
+    componentDidMount() {
+        this.initSheet();
     }
 
-    _goBack(){
+    _goBack() {
         window.location.hash = "#/"
     }
 
-    _handleNewTaskAdd(){
+    _handleNewTaskAdd() {
         let new_task = {};
-        new_task.id = task_count+1;
+        new_task.id = task_count + 1;
         task_count++;
         new_task.comment = "";
         new_task.pid = "";
@@ -40,24 +40,24 @@ export default class CreateSheet extends Component {
         new_task.status = "";
         let tasks = [...this.state.tasks];
         tasks.push(new_task);
-        this.setState( { tasks } );
+        this.setState({ tasks });
     }
 
-    _handleTaskChange(id,e){
+    _handleTaskChange(id, e) {
         let tasks = [...this.state.tasks];
-        tasks.forEach((task)=>{
-            if(task.id === id){
+        tasks.forEach((task) => {
+            if (task.id === id) {
                 task[e.target.name] = e.target.value;
             }
         });
         this.setState({ tasks });
     }
 
-    _handleNewTaskRemove(id,e){
+    _handleNewTaskRemove(id, e) {
         let tasks = [...this.state.tasks];
         let remainingTasks = [];
-        remainingTasks = tasks.filter((task)=>{return task.id !== id})       
-        this.setState({ tasks : remainingTasks });
+        remainingTasks = tasks.filter((task) => { return task.id !== id })
+        this.setState({ tasks: remainingTasks });
     }
 
 
@@ -70,74 +70,74 @@ export default class CreateSheet extends Component {
             method: 'GET',
             headers: myHeaders,
         }).then(res => res.json()).then(response => {
-            if (parseInt(response.success)) {                
-                this.setState({ 
+            if (parseInt(response.success)) {
+                this.setState({
                     date: response.data.date,
                     projects: response.data.projects
-                 });
+                });
             }
             else {
                 toast.error(response.msg);
             }
         })
     }
-    
-    _submitSheet(e){
+
+    _submitSheet(e) {
         e.preventDefault();
         let API = SERVER + '/newSheet'
         var formData = new FormData();
         var myHeaders = new Headers();
         myHeaders.append("Authorization", localStorage.getItem('token'));
-        formData.append('tasks', JSON.stringify(this.state.tasks));  
-        formData.append('date', this.state.date);      
+        formData.append('tasks', JSON.stringify(this.state.tasks));
+        formData.append('date', this.state.date);
         fetch(API, {
             method: 'POST',
             headers: myHeaders,
             body: formData,
         })
-        .then(res => res.json()).then(response => {            
-            if (parseInt(response.success)) {
-                toast.success("Sheet created successfully");
-            }
-            else {
-                toast.error(response.msg);
-            }
-        })
+            .then(res => res.json()).then(response => {
+                if (parseInt(response.success)) {
+                    toast.success("Sheet created successfully");
+                }
+                else {
+                    toast.error(response.msg);
+                }
+            })
     }
 
-    TopBar(){
-        return(
+    TopBar() {
+        return (
             <div className="container-fluid">
                 <div className="row top-bar">
-                    <button onClick={()=>this._goBack()} className="btn btn-primary mgn-lf"> ← Go Back </button>
+                    <button onClick={() => this._goBack()} className="btn btn-primary mgn-lf"> ← Go Back </button>
                 </div>
             </div>
         )
     }
 
-  
 
-    SheetView(){
-        return(
+
+    SheetView() {
+        return (
             <div className="container">
                 <div className="padded-section">
-                    <h2 className="page-title"> 
+                    <h2 className="page-title">
                         <i className="icon-c">
-                            <svg fill="currentColor" style={{display: 'inline-block', verticalAlign: 'middle'}}height={32}width={32}viewBox="0 0 16 16"><path d="M5 6h2v2h-2zM8 6h2v2h-2zM11 6h2v2h-2zM2 12h2v2h-2zM5 12h2v2h-2zM8 12h2v2h-2zM5 9h2v2h-2zM8 9h2v2h-2zM11 9h2v2h-2zM2 9h2v2h-2zM13 0v1h-2v-1h-7v1h-2v-1h-2v16h15v-16h-2zM14 15h-13v-11h13v11z"/></svg>
-                        </i>   
+                            <svg fill="currentColor" style={{ display: 'inline-block', verticalAlign: 'middle' }} height={32} width={32} viewBox="0 0 16 16"><path d="M5 6h2v2h-2zM8 6h2v2h-2zM11 6h2v2h-2zM2 12h2v2h-2zM5 12h2v2h-2zM8 12h2v2h-2zM5 9h2v2h-2zM8 9h2v2h-2zM11 9h2v2h-2zM2 9h2v2h-2zM13 0v1h-2v-1h-7v1h-2v-1h-2v16h15v-16h-2zM14 15h-13v-11h13v11z" /></svg>
+                        </i>
                         <span>Today's Sheet</span>
-                    </h2>  
+                    </h2>
                 </div>
                 {this.Tasks()}
                 <div className="row">
                     <div className="col-md-12 col-sm-12 col-lg-12">
-                        <div className="padded-section">                                                     
-                            {this.state.tasks.length?
-                                <button onClick={this._submitSheet.bind(this)} className="btn btn-primary flt-right mgn-bt sm-margin"> Save </button>                                                  
+                        <div className="padded-section">
+                            {this.state.tasks.length ?
+                                <button onClick={this._submitSheet.bind(this)} className="btn btn-primary flt-right mgn-bt sm-margin"> Save </button>
                                 :
                                 ""
                             }
-                            <button onClick={this._handleNewTaskAdd.bind(this)} className="btn btn-primary flt-right mgn-bt"> Add Task </button>                                                  
+                            <button onClick={this._handleNewTaskAdd.bind(this)} className="btn btn-primary flt-right mgn-bt"> Add Task </button>
                         </div>
                     </div>
                 </div>
@@ -167,7 +167,7 @@ export default class CreateSheet extends Component {
                                             value={task.pid}
                                             onChange={this._handleTaskChange.bind(this, task.id)}
                                         >
-                                            <option value={""} disabled> Project </option>                                            
+                                            <option value={""} disabled> Project </option>
                                             {this.state.projects.length ?
                                                 this.state.projects.map((project, index_2) => (
                                                     <option key={index_2} value={project.id}>{project.name}</option>
@@ -210,8 +210,8 @@ export default class CreateSheet extends Component {
                                         />
                                     </div>
                                     <div>
-                                        <i className="icon-c delete" onClick={this._handleNewTaskRemove.bind(this,task.id)}>
-                                            <svg fill="currentColor" style={{display: 'inline-block', verticalAlign: 'middle'}}height={32}width={32}viewBox="0 0 20 20"><path d="M14.348,14.849c-0.469,0.469-1.229,0.469-1.697,0L10,11.819l-2.651,3.029c-0.469,0.469-1.229,0.469-1.697,0c-0.469-0.469-0.469-1.229,0-1.697l2.758-3.15L5.651,6.849c-0.469-0.469-0.469-1.228,0-1.697c0.469-0.469,1.228-0.469,1.697,0L10,8.183l2.651-3.031c0.469-0.469,1.228-0.469,1.697,0c0.469,0.469,0.469,1.229,0,1.697l-2.758,3.152l2.758,3.15C14.817,13.62,14.817,14.38,14.348,14.849z"/></svg>
+                                        <i className="icon-c delete" onClick={this._handleNewTaskRemove.bind(this, task.id)}>
+                                            <svg fill="currentColor" style={{ display: 'inline-block', verticalAlign: 'middle' }} height={32} width={32} viewBox="0 0 20 20"><path d="M14.348,14.849c-0.469,0.469-1.229,0.469-1.697,0L10,11.819l-2.651,3.029c-0.469,0.469-1.229,0.469-1.697,0c-0.469-0.469-0.469-1.229,0-1.697l2.758-3.15L5.651,6.849c-0.469-0.469-0.469-1.228,0-1.697c0.469-0.469,1.228-0.469,1.697,0L10,8.183l2.651-3.031c0.469-0.469,1.228-0.469,1.697,0c0.469,0.469,0.469,1.229,0,1.697l-2.758,3.152l2.758,3.15C14.817,13.62,14.817,14.38,14.348,14.849z" /></svg>
                                         </i>
                                     </div>
                                 </div>
@@ -230,7 +230,7 @@ export default class CreateSheet extends Component {
             <div className="Timesheet">
                 {this.TopBar()}
                 {this.SheetView()}
-                <Toaster toastOptions={{className: 'Toast_Class'}}/>
+                <Toaster toastOptions={{ className: 'Toast_Class' }} />
             </div>
         )
     }
--- client/src/ViewTimeSheet/ViewTimeSheet.js
+++ client/src/ViewTimeSheet/ViewTimeSheet.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
 import Table from 'react-table-lite';
 import { SERVER } from './../assets/scripts/constants';
@@ -9,17 +9,17 @@ import '../assets/css/grid.min.css';
 export default class ViewTimeSheet extends Component {
 
     constructor(props) {
-        super(props)    
-        this.state = {                          
-             tasks:[],             
+        super(props)
+        this.state = {
+            tasks: [],
         }
     }
-    
-    componentDidMount(){
-        this.getSheetInfo();        
+
+    componentDidMount() {
+        this.getSheetInfo();
     }
 
-    _goBack(){
+    _goBack() {
         window.location.hash = "#/timesheets"
     }
 
@@ -27,16 +27,16 @@ export default class ViewTimeSheet extends Component {
         let API = SERVER + '/getTimesheet';
         const { sid } = this.props.match.params;
         var formData = new FormData();
-        formData.append("sid",sid);
+        formData.append("sid", sid);
         var myHeaders = new Headers();
-        myHeaders.append("Authorization", localStorage.getItem('token'));        
+        myHeaders.append("Authorization", localStorage.getItem('token'));
         fetch(API, {
             method: 'POST',
             body: formData,
             headers: myHeaders,
         }).then(res => res.json()).then(response => {
-            if (parseInt(response.success)) {                
-                let tasks = response.data.length? response.data : []
+            if (parseInt(response.success)) {
+                let tasks = response.data.length ? response.data : []
                 this.setState({ tasks })
             }
             else {
@@ -44,31 +44,31 @@ export default class ViewTimeSheet extends Component {
             }
         })
     }
-    
-  
-    TopBar(){
-        return(
+
+
+    TopBar() {
+        return (
             <div className="container-fluid">
                 <div className="row top-bar">
-                    <button onClick={()=>this._goBack()} className="btn btn-primary mgn-lf"> ← Go Back </button>
+                    <button onClick={() => this._goBack()} className="btn btn-primary mgn-lf"> ← Go Back </button>
                 </div>
             </div>
         )
     }
 
-  
-    SheetView(){
-        return(
+
+    SheetView() {
+        return (
             <div className="container">
                 <div className="padded-section">
-                    <h2 className="page-title"> 
+                    <h2 className="page-title">
                         <i className="icon-c">
-                            <svg fill="currentColor" style={{display: 'inline-block', verticalAlign: 'middle'}}height={32}width={32}viewBox="0 0 16 16"><path d="M5 6h2v2h-2zM8 6h2v2h-2zM11 6h2v2h-2zM2 12h2v2h-2zM5 12h2v2h-2zM8 12h2v2h-2zM5 9h2v2h-2zM8 9h2v2h-2zM11 9h2v2h-2zM2 9h2v2h-2zM13 0v1h-2v-1h-7v1h-2v-1h-2v16h15v-16h-2zM14 15h-13v-11h13v11z"/></svg>
-                        </i>   
+                            <svg fill="currentColor" style={{ display: 'inline-block', verticalAlign: 'middle' }} height={32} width={32} viewBox="0 0 16 16"><path d="M5 6h2v2h-2zM8 6h2v2h-2zM11 6h2v2h-2zM2 12h2v2h-2zM5 12h2v2h-2zM8 12h2v2h-2zM5 9h2v2h-2zM8 9h2v2h-2zM11 9h2v2h-2zM2 9h2v2h-2zM13 0v1h-2v-1h-7v1h-2v-1h-2v16h15v-16h-2zM14 15h-13v-11h13v11z" /></svg>
+                        </i>
                         <span>Time Sheet</span>
-                    </h2>  
+                    </h2>
                 </div>
-                {this.Tasks()}             
+                {this.Tasks()}
             </div>
         )
     }
@@ -87,25 +87,25 @@ export default class ViewTimeSheet extends Component {
                                             placeholder="name"
                                             value={task.task}
                                             disabled={true}
-                                            onChange={e=> {} }
+                                            onChange={e => { }}
                                         />
                                     </div>
                                     <div className="col-md-3 col-sm-6">
                                         <input
-                                            name="project"                                            
+                                            name="project"
                                             placeholder="project"
                                             value={task.project}
                                             disabled={true}
-                                            onChange={e=> {} }
+                                            onChange={e => { }}
                                         />
                                     </div>
                                     <div className="col-md-3 col-sm-6">
                                         <input
-                                            name="hours"                                            
+                                            name="hours"
                                             placeholder="hours"
                                             value={task.hours}
                                             disabled={true}
-                                            onChange={e=> {} }
+                                            onChange={e => { }}
                                         />
                                     </div>
                                     <div className="col-md-3 col-sm-6" >
@@ -113,7 +113,7 @@ export default class ViewTimeSheet extends Component {
                                             name="status"
                                             value={task.status}
                                             disabled={true}
-                                            onChange={e=> {} }
+                                            onChange={e => { }}
                                         >
                                             <option value={""} disabled> Status </option>
                                             <option value={1}> In Progress </option>
@@ -129,9 +129,9 @@ export default class ViewTimeSheet extends Component {
                                             placeholder="comments"
                                             value={task.comment}
                                             disabled={true}
-                                            onChange={e=> {} }
+                                            onChange={e => { }}
                                         />
-                                    </div>                          
+                                    </div>
                                 </div>
                             </div>
                         )
@@ -148,7 +148,7 @@ export default class ViewTimeSheet extends Component {
             <div className="Timesheet">
                 {this.TopBar()}
                 {this.SheetView()}
-                <Toaster toastOptions={{className: 'Toast_Class'}}/>
+                <Toaster toastOptions={{ className: 'Toast_Class' }} />
             </div>
         )
     }
